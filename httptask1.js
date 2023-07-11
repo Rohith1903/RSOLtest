1df4d0d56bc6c62702a154ad6505f2c498979c9e
Rohith Vijaey <55175422+Rohith1903@users.noreply.github.com>
2023-07-11 20:21:59 +0530

    Update httptask1.js

--- httptask1.js
+++ httptask1.js
@@ -6,7 +6,7 @@ let handleRequest = (request, response) => {
 	response.writeHead(200, {
 		'content-Type': 'text/txt'
 	});
-	fs.readFile('Trial.txt', null, function (error, data) {
+	fs.readFile('Trialhttptask1.txt', null, function (error, data) {
 		if (error) {
 			response.writeHead(404);
 			response.write('file not found');
